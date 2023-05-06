778e2d3126c6cdf403a33b598d929c4ec5c66fdd
sumit anil tingne <80904144+sumittingnae@users.noreply.github.com>
2023-05-06 19:22:13 +0530

    Add files via upload

--- src/service/service.jsx
+++ src/service/service.jsx
@@ -1,23 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './service.scss';
 import Header from "../header/header";
+import { slist } from './slist';
+
 
 function Service (){
+  const [service, setService] = useState(slist);
+
+  useEffect(()=>{
+    setService(slist);
+  })
+    
 
     return (
       <>
-      <Header/>
+        <Header />
         <div className="service">
           <div className="container">
             <div className="col-12">
               <div className="d-flex w-100 col-lg-5">
                 <div className="wrappe">
-                  <img src="./404.gif" alt="" />
-                  
+                  <div className="swiper MySwiper container">
+                    <div className="swiper-wrapper content">
+                      {service.map((data) => {
+                        const { name, Department, image } = data;
+                        return(
+                        <div className="swiper-slide card">
+                          <div className="card-content">
+                            <div className="image">
+                              <img src={image} alt="" />
+                            </div>
+                            <div className="media-icons">
+                              <i className="fab fa-facebook"></i>
+                              <i className="fab fa-twitter"></i>
+                              <i className="fab fa-github"></i>
+                            </div>
+                            <div className="name-profession">
+                              <span className="name">{name}</span>
+                              <span className="profession">{Department}</span>
+                            </div>
+                            <div className="rating">
+                              <i className="fa fa-star"></i>
+                              <i className="fa fa-star"></i>
+                              <i className="fa fa-star"></i>
+                              <i className="fa fa-star"></i>
+                              <i className="fa fa-star"></i>
+                            </div>
+                            <div className="button">
+                              <button className="anoutMe">About Me</button>
+                              <button className="view">view</button>
+                            </div>
+                          </div>
+                        </div>);
+                      })}
+                    </div>
+
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-pagination"></div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
         </div>
       </>
     );
---
+++ src/service/slist.js
@@ -0,0 +1,32 @@
+ export const slist = [
+   {
+     id: 1,
+     name: "Blood Test",
+     Department: "Pathology",
+     image: "./pathology.jpg",
+   },
+   {
+     id: 2,
+     name: " Heart Speclist",
+     Department: "surgenes",
+     image: "./heart.jpg",
+   },
+   {
+     id: 3,
+     name: "24x7 Pharmacy",
+     profession: "Medicin",
+     image: "./pharmacy.jpg",
+   },
+   {
+     id: 4,
+     name: " Heart Speclist",
+     Department: "surgenes",
+     image: "./heart.jpg",
+   },
+   {
+     id: 5,
+     name: "24x7 Pharmacy",
+     profession: "Medicin",
+     image: "./pharmacy.jpg",
+   },
+ ];
